adf7c134fc8dc8e7a76783c72ac58319df2099c9
Daniel Tschertkow <daniel.tschertkow@rohde-schwarz.com>
2025-07-30 09:54:50 +0200

    Fix errors in Q10 cake layer

--- keyboards/keychron/q10/iso_encoder/keymaps/cake/keymap.c
+++ keyboards/keychron/q10/iso_encoder/keymaps/cake/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_GRV,   DE_1,     DE_2,     DE_3,     DE_4,     DE_5,      DE_6,     DE_7,     DE_8,     DE_9,     DE_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   DE_Q,     DE_W,     DE_E,     DE_R,     DE_T,      DE_Z,     DE_U,     DE_I,     DE_O,     DE_P,     KC_LBRC,  KC_RBRC,                      KC_PGDN,
         _______,  KC_CAPS,  DE_A,     DE_S,     DE_D,     DE_F,     DE_G,      DE_H,     DE_J,     DE_K,     DE_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,  DE_NUBS,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        _______,  KC_LSFT,  DE_LABK,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LOPT,            KC_LCMD,  KC_SPC,  MO(MAC_FN),                     KC_SPC,             KC_RCMD,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_iso_90(
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_GRV,   DE_1,     DE_2,     DE_3,     DE_4,     DE_5,      DE_6,     DE_7,     DE_8,     DE_9,     DE_0,     DE_SS,    DE_ACUT,  KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   DE_Q,     DE_W,     DE_E,     DE_R,     DE_T,      DE_Z,     DE_U,     DE_I,     DE_O,     DE_P,     DE_UDIA,  DE_PLUS,                      KC_PGDN,
         _______,  KC_CAPS,  DE_A,     DE_S,     DE_D,     DE_F,     DE_G,      DE_H,     DE_J,     DE_K,     DE_L,     DE_ODIA,  DE_ADIA,  DE_HASH,  KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,  DE_NUBS,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,  KC_RSFT,  KC_UP,
+        _______,  KC_LSFT,  DE_LABK,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,  KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,  MO(WIN_FN),                     KC_SPC,             KC_RALT,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_GRV,   DE_1,     DE_2,     DE_3,     DE_4,     DE_5,      DE_6,     DE_7,     DE_8,     DE_9,     DE_0,     DE_SS,    DE_ACUT,  KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   DE_Q,     DE_W,     DE_E,     DE_R,     DE_T,      DE_Z,     DE_U,     DE_I,     DE_O,     DE_P,     DE_BSLS,  DE_PLUS,                      KC_PGDN,
         _______,  KC_CAPS,  DE_A,     DE_S,     DE_D,     DE_F,     DE_G,      DE_H,     DE_J,     DE_K,     DE_L,     DE_LBRC,  DE_RBRC,  DE_HASH,  KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,  DE_NUBS,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,  KC_RSFT,  KC_UP,
+        _______,  KC_LSFT,  DE_LABK,  DE_Y,     DE_X,     DE_C,     DE_V,      DE_B,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,  KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,  MO(WIN_FN),                     KC_SPC,             KC_RALT,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 
@@ -88,14 +88,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-#if defined(ENCODER_MAP_ENABLE)
+#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
-    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_PROG] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU), ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [MAC_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_PROG] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU) },
 };
+
 
 const key_override_t left_bracket_override = ko_make_basic(MOD_MASK_SHIFT, DE_LBRC, DE_LCBR);
 const key_override_t right_bracket_override = ko_make_basic(MOD_MASK_SHIFT, DE_RBRC, DE_RCBR);
